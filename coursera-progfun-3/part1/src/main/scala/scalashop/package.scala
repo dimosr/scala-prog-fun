7630bcfac162efcd59cb347961e8f4c7c018eb27
Raptis Dimos <raptis.dimos@yahoo.gr>
2017-10-31 20:23:33 +0000

    Add part1 for third section

---
+++ coursera-progfun-3/part1/src/main/scala/scalashop/package.scala
@@ -0,0 +1,65 @@
+
+import common._
+
+package object scalashop {
+
+  /** The value of every pixel is represented as a 32 bit integer. */
+  type RGBA = Int
+
+  /** Returns the red component. */
+  def red(c: RGBA): Int = (0xff000000 & c) >>> 24
+
+  /** Returns the green component. */
+  def green(c: RGBA): Int = (0x00ff0000 & c) >>> 16
+
+  /** Returns the blue component. */
+  def blue(c: RGBA): Int = (0x0000ff00 & c) >>> 8
+
+  /** Returns the alpha component. */
+  def alpha(c: RGBA): Int = (0x000000ff & c) >>> 0
+
+  /** Used to create an RGBA value from separate components. */
+  def rgba(r: Int, g: Int, b: Int, a: Int): RGBA = {
+    (r << 24) | (g << 16) | (b << 8) | (a << 0)
+  }
+
+  /** Restricts the integer into the specified range. */
+  def clamp(v: Int, min: Int, max: Int): Int = {
+    if (v < min) min
+    else if (v > max) max
+    else v
+  }
+
+  /** Image is a two-dimensional matrix of pixel values. */
+  class Img(val width: Int, val height: Int, private val data: Array[RGBA]) {
+    def this(w: Int, h: Int) = this(w, h, new Array(w * h))
+    def apply(x: Int, y: Int): RGBA = data(y * width + x)
+    def update(x: Int, y: Int, c: RGBA): Unit = data(y * width + x) = c
+  }
+
+  /** Computes the blurred RGBA value of a single pixel of the input image. */
+  def boxBlurKernel(src: Img, x: Int, y: Int, radius: Int): RGBA = {
+    val neighbours = for {
+      tempX <- (x-radius) to (x+radius)
+      tempY <- (y-radius) to (y+radius)
+      if clamp(tempX, 0, src.width-1) == tempX && clamp(tempY, 0, src.height-1) == tempY
+    } yield src(clamp(tempX, 0, src.width-1), clamp(tempY, 0, src.height-1))
+
+    if(neighbours.isEmpty)
+      src(x, y)
+    else {
+      val (totalRed, totalGreen, totalBlue, totalAlpha) = neighbours
+        .map(pixel => (red(pixel), green(pixel), blue(pixel), alpha(pixel)))
+        .reduce((t1, t2) => (t1._1 + t2._1, t1._2 + t2._2, t1._3 + t2._3, t1._4 + t2._4))
+
+      rgba(
+        totalRed/neighbours.length,
+        totalGreen/neighbours.length,
+        totalBlue/neighbours.length,
+        totalAlpha/neighbours.length
+      )
+    }
+
+  }
+
+}
